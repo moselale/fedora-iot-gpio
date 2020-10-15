d99c84a464f1a7aa6c2fe6f422203675304eb843
silvandrici <silvandrici@gmail.com>
2020-10-15 18:51:55 +0200

    main added

--- src/main.py
+++ src/main.py
@@ -14,11 +14,18 @@ scorrevole.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=
 portone = chip.get_lines([22])
 portone.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
 i = 4
+
+def getOpposite(val):
+    if val == 0:
+        return 1
+    else:
+        return 0
+
 while i>0:
-    sbarra.set_values([not sbarra.get_values()])
-    scorrevole.set_values([not scorrevole.get_values()])
-    portone.set_values([not portone.get_values()])
-    print("i: " , i)
+    sbarra.set_values([getOpposite(sbarra.get_values())])
+    print()
+    scorrevole.set_values([getOpposite(scorrevole.get_values())])
+    portone.set_values([getOpposite(portone.get_values())])
     i = i-1
     time.sleep(1)
 
---
+++ src/test.py
@@ -0,0 +1,19 @@
+import time
+
+value = 0
+i = 4
+
+def getOpposite(val):
+    if val == 0:
+        return 1
+    else:
+        return 0
+
+while i>0:
+    i = i-1
+    print(i)
+    print(value)
+    print(getOpposite(value))
+
+    time.sleep(1)
+
