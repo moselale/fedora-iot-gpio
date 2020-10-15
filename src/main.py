4d0d35543ff52c997755e103ef47ff6b28baf6b2
silvandrici <silvandrici@gmail.com>
2020-10-15 18:31:28 +0200

    main added

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ pinPortone = 22
 
 chip = gpiod.Chip('gpiochip0')
 
-sbarra = chip.get_lines([pinsbarra])
+sbarra = chip.get_lines([17])
 sbarra.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
-scorrevole = chip.get_lines([pinScorrevole])
+scorrevole = chip.get_lines([27])
 scorrevole.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
-portone = chip.get_lines([pinPortone])
+portone = chip.get_lines([22])
 portone.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
 i = 4
 while i>0:
