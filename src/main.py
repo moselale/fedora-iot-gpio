1b9c3e75003d4e2a18058ac6ed8f5e4d32eaf386
silvandrici <silvandrici@gmail.com>
2020-10-16 10:14:12 +0200

    main added

--- src/main.py
+++ src/main.py
@@ -13,52 +13,63 @@ pinSbarra = 17
 pinScorrevole = 27
 pinPortone = 22
 
-
-# scorrevole = chip.get_lines([27])
-# scorrevole.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
-# portone = chip.get_lines([22])
-# portone.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
-# i = 4
-
 def getOpposite(val):
     if val == 0:
         return 1
     else:
         return 0
 
-# while i>0:
-#     sbarra.set_values([getOpposite(sbarra.get_values()[0])])
-#     print(getOpposite(sbarra.get_values()[0]))
-#     scorrevole.set_values([getOpposite(scorrevole.get_values()[0])])
-#     portone.set_values([getOpposite(portone.get_values()[0])])
-#     i = i-1
-#     time.sleep(1)
-
-
 class Sbarra(Resource):
     def get(self):
-        # if not sbarraRequested:
-        #     sbarraRequested = True
         try:
-            sbarra.request(consumer='on', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
+            sbarra.request(consumer='sbarra', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
         except:
             print()
     #    sbarra.set_values([getOpposite(sbarra.get_values()[0])])
         sbarra.set_values([0])
         time.sleep(4)
         sbarra.set_values([1])
-        return {'oppened': "yes"}
+        return {'sbarra': "yes"}
+
+class Scorrevole(Resource):
+    def get(self):
+        try:
+            scorrevole.request(consumer='scorrevole', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
+        except:
+            print()
+    #    scorrevole.set_values([getOpposite(scorrevole.get_values()[0])])
+        scorrevole.set_values([0])
+        time.sleep(4)
+        scorrevole.set_values([1])
+        return {'scorrevole': "yes"}
+
+class Portone(Resource):
+    def get(self):
+        try:
+            portone.request(consumer='portone', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[ 1 ])
+        except:
+            print()
+    #    portone.set_values([getOpposite(portone.get_values()[0])])
+        portone.set_values([0])
+        time.sleep(4)
+        portone.set_values([1])
+        return {'portone': "yes"}
+
 
 api.add_resource(Sbarra, '/sbarra')
-# api.add_resource(PollutionResource, '/pollution')
-# api.add_resource(GasResource, '/gas')
+api.add_resource(Scorrevole, '/scorrevole')
+api.add_resource(Portone, '/portone')
+
 
 print(__name__)
 if __name__ == '__main__':
     print("Starting process...")
     
     chip = gpiod.Chip('gpiochip0')
+    
     sbarra = chip.get_lines([17])
-    # sbarraRequested = False
+    scorrevole = chip.get_lines([27])
+    portone = chip.get_lines([22])
+
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
