90d99fa574fdab08a7da9a7e3374dee53ab29942
silvandrici <silvandrici@gmail.com>
2020-10-21 12:48:37 +0200

    main added

--- src/main.py
+++ src/main.py
@@ -67,9 +67,9 @@ if __name__ == '__main__':
     
     chip = gpiod.Chip('gpiochip0')
 
-    sbarra = chip.get_lines([14])
-    scorrevole = chip.get_lines([15])
-    portone = chip.get_lines([23])
+    sbarra = chip.get_lines([23])
+    scorrevole = chip.get_lines([24])
+    portone = chip.get_lines([25])
 
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
